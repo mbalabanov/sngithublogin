942adeebc13739b96ba1d6f9fe62b936ab2e5306
Marin Balabanov <marincomics@gmail.com>
2019-12-12 16:28:47 +0100

    Removed files

--- secrets.py
+++
@@ -1,8 +0,0 @@
-# this file MUST be in .gitignore
-# do not upload it to GitHub!
-# if you upload it to GitHub, someone can steal your GitHub ID and secret!
-import os
-
-os.environ["GITHUB_CLIENT_ID"] = "9fd031e90bcc151a41f1"
-os.environ["GITHUB_CLIENT_SECRET"] = "db91ec0946eb31a2a1f54b7a75b97a76d99ca835"
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
